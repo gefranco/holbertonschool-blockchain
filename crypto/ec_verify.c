6cccab5a46855d6c0265197604fadd493c8a1ab8
gefranco <933@holbertonschool.com>
2021-07-22 17:08:29 +0000

    [fix]

--- crypto/ec_verify.c
+++ crypto/ec_verify.c
@@ -4,7 +4,7 @@ int ec_verify(EC_KEY const *key,
 {
 	if (!key || !msg || !sig)
 		return (0);
-	if (!ECDSA_verify(0, msg, msglen, sig->sig, sig->len, (EC_KEY *)key))
+	if (ECDSA_verify(0, msg, msglen, sig->sig, sig->len, (EC_KEY *)key) != 1)
 		return (0);
 	return (1);
 }
