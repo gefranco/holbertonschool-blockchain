299a045e2d6307effc9f72db76bb5bb20862ebdf
gefranco <933@holbertonschool.com>
2021-07-22 15:56:36 +0000

    [fix] add openssl/pem.h and remove line

--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -1,38 +1,31 @@
 #ifndef _HBT_CRYPTO_
 #define _HBT_CRYPTO_
+#include <openssl/sha.h>
+#include <openssl/ec.h>
+#include <openssl/obj_mac.h>
+#include <openssl/ecdsa.h>
+#include <openssl/pem.h>
+#include <stdint.h>
+#include <stddef.h>
 
-# include <openssl/sha.h>
-# include <stdint.h>
-# include <openssl/ec.h>
-# include <openssl/obj_mac.h>
-# include <openssl/ecdsa.h>
-# define EC_CURVE   NID_secp256k1
+#define EC_CURVE NID_secp256k1
 
-/* EC_KEY public key octet string length (using 256-bit curve) */
 # define EC_PUB_LEN 65
-/* Maximum signature octet string length (using 256-bit curve) */
-# define SIG_MAX_LEN    72
+# define SIG_MAX_LEN 72
 
-# define PRI_FILENAME   "key.pem"
-# define PUB_FILENAME   "key_pub.pem"
+# define PRI_FILENAME "key.pem"
+# define PUB_FILENAME "key_pub.pem"
 
 /**
- * struct sig_s - EC Signature structure
- *
- * @sig: Signature buffer. The whole space may not be used
- * @len: Actual signature size.
- * Can't exceed SIG_MAX_LEN, therefore stored on a byte
+ * struct sig_s - signature structure
+ * @sig: signature
+ * @len: signature length
  */
 typedef struct sig_s
 {
-    /*
-     * @sig must stay first, so we can directly use the structure as
-     * an array of char
-     */
-	uint8_t     sig[SIG_MAX_LEN];
-	uint8_t     len;
+	uint8_t sig[SIG_MAX_LEN];
+	uint8_t len;
 } sig_t;
-
 
 uint8_t *sha256(int8_t const *s, size_t len,
 			uint8_t digest[SHA256_DIGEST_LENGTH]);
