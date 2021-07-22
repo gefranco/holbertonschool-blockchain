7c01a61c2200a801f7840476578c0b5eee4f219d
gefranco <933@holbertonschool.com>
2021-07-22 16:58:20 +0000

    [fix] set sign len to 0 when fail

--- crypto/ec_sign.c
+++ crypto/ec_sign.c
@@ -10,8 +10,10 @@ uint8_t *ec_sign(EC_KEY const *key,
 
 	bzero(sig->sig, sizeof(sig->sig));
 	if (!ECDSA_sign(0, msg, msglen, sig->sig, &len, (EC_KEY *)key))
+	{
+		sig->len = 0;
 		return (NULL);
-
+	}
 	sig->len = (uint8_t)len;
 	return ((uint8_t *)sig->sig);
 }
