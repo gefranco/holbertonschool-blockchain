ab016f870c0f225370c0ca8ce1839c0cbfb2fcb0
gefranco <933@holbertonschool.com>
2021-07-22 16:56:34 +0000

    [add] function that signs a given set of bytes, using a given EC_KEY private key

---
+++ crypto/ec_sign.c
@@ -0,0 +1,17 @@
+#include "hblk_crypto.h"
+#include <strings.h>
+uint8_t *ec_sign(EC_KEY const *key,
+			uint8_t const *msg, size_t msglen, sig_t *sig)
+{
+	uint32_t len = 0;
+
+	if (!key || !msg)
+		return (NULL);
+
+	bzero(sig->sig, sizeof(sig->sig));
+	if (!ECDSA_sign(0, msg, msglen, sig->sig, &len, (EC_KEY *)key))
+		return (NULL);
+
+	sig->len = (uint8_t)len;
+	return ((uint8_t *)sig->sig);
+}
