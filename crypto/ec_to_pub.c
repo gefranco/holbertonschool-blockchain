239f9acc7558e58799fae5e4f2b78d89a2b9c131
gefranco <933@holbertonschool.com>
2021-07-22 15:24:14 +0000

    [add] function that extracts the public key from an EC_KEY opaque structure

---
+++ crypto/ec_to_pub.c
@@ -0,0 +1,27 @@
+#include "hblk_crypto.h"
+
+uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN])
+{
+	BN_CTX *context = NULL;
+	const EC_GROUP *ec_group = NULL;
+	const EC_POINT *ec_point = NULL;
+	size_t len = 0;
+
+	if (!key)
+		return (NULL);
+
+	context = BN_CTX_new();
+	ec_group = EC_KEY_get0_group(key);
+	ec_point = EC_KEY_get0_public_key(key);
+
+	len = EC_POINT_point2oct(ec_group, ec_point,
+					(point_conversion_form_t) POINT_CONVERSION_UNCOMPRESSED,
+						NULL, EC_PUB_LEN, context);
+	if (!EC_POINT_point2oct(ec_group, ec_point,
+					(point_conversion_form_t) POINT_CONVERSION_UNCOMPRESSED,
+						(unsigned char *)pub, len, context))
+		return (NULL);
+	BN_CTX_free(context);
+
+	return (pub);
+}
