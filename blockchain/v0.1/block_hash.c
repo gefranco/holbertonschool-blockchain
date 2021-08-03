7b5354a96f016f89fba72200883336ab131fa108
gefranco <933@holbertonschool.com>
2021-08-03 22:39:52 +0000

    [fix] data len

--- blockchain/v0.1/block_hash.c
+++ blockchain/v0.1/block_hash.c
@@ -4,7 +4,7 @@
 uint8_t *block_hash(block_t const *block,
 			uint8_t hash_buf[SHA256_DIGEST_LENGTH])
 {
-	size_t len = sizeof(block->info) + sizeof(block->data.len);
+	size_t len = sizeof(block->info) + block->data.len;
 
 	return (sha256((int8_t *)block, len, hash_buf));
 }
