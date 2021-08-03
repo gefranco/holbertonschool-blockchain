3f08eab6e2f94cd3a751c493d6dd931b93a8e910
gefranco <933@holbertonschool.com>
2021-08-03 22:31:03 +0000

    [add] function that computes the hash of a Block

---
+++ blockchain/v0.1/block_hash.c
@@ -0,0 +1,10 @@
+#include "blockchain.h"
+#include <stdlib.h>
+#include "../../crypto/hblk_crypto.h"
+uint8_t *block_hash(block_t const *block,
+			uint8_t hash_buf[SHA256_DIGEST_LENGTH])
+{
+	size_t len = sizeof(block->info) + sizeof(block->data);
+
+	return (sha256((int8_t *)block, len, hash_buf));
+}
