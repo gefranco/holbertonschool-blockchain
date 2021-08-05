7cf3aa06cd2068946ceed88197e0373d050843b2
gefranco <933@holbertonschool.com>
2021-08-05 21:44:31 +0000

    [fix]betty

--- blockchain/v0.2/block_is_valid.c
+++ blockchain/v0.2/block_is_valid.c
@@ -45,9 +45,9 @@ int block_is_valid(block_t const *block, block_t const *prev_block)
 		return (1);
 	if (block->data.len > BLOCKCHAIN_DATA_MAX)
 		return (1);
-	
+
 	if (!hash_matches_difficulty(block->hash, block->info.difficulty))
 		return (1);
-	return(0);
+	return (0);
 
 }
