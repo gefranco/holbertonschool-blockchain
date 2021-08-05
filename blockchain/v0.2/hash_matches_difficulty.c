e3e712b6543e8b9afb022ff9e9ec97234804061e
gefranco <933@holbertonschool.com>
2021-08-05 15:34:57 +0000

    [add] function that checks whether a given hash matches a given difficulty

---
+++ blockchain/v0.2/hash_matches_difficulty.c
@@ -0,0 +1,30 @@
+#include "blockchain.h"
+#include <stdio.h>
+int hash_matches_difficulty(uint8_t const hash[SHA256_DIGEST_LENGTH], uint32_t difficulty)
+{
+	int i;
+	int dif = 1;
+	uint8_t *ptr = (uint8_t *)hash;
+	uint32_t difficulty_calc = 0;
+	(void)difficulty;	
+	
+	while (ptr < hash + SHA256_DIGEST_LENGTH && dif)
+	{
+		for (i = 7; i >= 0; i--)
+		{
+			
+			
+			if((*ptr >> i) & 1)
+			{
+				dif = 0;
+				break;
+			}
+			difficulty_calc++;
+		}
+		ptr++;
+	}
+
+	if (difficulty_calc < difficulty)
+		return 0;
+	return 1;
+}
