e4fe854157ea1c8e2847653a301db5c84c5f4c60
gefranco <933@holbertonschool.com>
2021-08-05 21:49:03 +0000

    [fix] betty

--- blockchain/v0.2/block_mine.c
+++ blockchain/v0.2/block_mine.c
@@ -1,12 +1,16 @@
 #include "blockchain.h"
 #include <stdio.h>
+/**
+ * block_mine - mines a Block in order to insert it in the Blockchain
+ * @block: the Block to be mined
+ */
 void block_mine(block_t *block)
 {
 	uint64_t nonce = 0;
-	do
-	{
+
+	do {
 		block->info.nonce = nonce;
 		block_hash(block, block->hash);
-		nonce ++;
-	}while (!hash_matches_difficulty(block->hash,block->info.difficulty));
+		nonce++;
+	} while (!hash_matches_difficulty(block->hash, block->info.difficulty));
 }
