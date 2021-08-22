18a0e9bcef78c77b220f5800647ec0447cccbdd7
gefranco <933@holbertonschool.com>
2021-08-22 22:44:44 +0000

    [fix] hashcontent length

--- blockchain/v0.3/transaction/tx_out_create.c
+++ blockchain/v0.3/transaction/tx_out_create.c
@@ -18,7 +18,8 @@ tx_out_t *tx_out_create(uint32_t amount, uint8_t const pub[EC_PUB_LEN])
 
 	tx_out->amount = amount;
 	memcpy(tx_out->pub, pub, EC_PUB_LEN);
-	if (!sha256((const int8_t *)tx_out, SHA256_DIGEST_LENGTH, tx_out->hash))
+	if (!sha256((const int8_t *)tx_out,
+		(sizeof(uint32_t) + EC_PUB_LEN), tx_out->hash))
 	{
 		free(tx_out);
 		return (NULL);
