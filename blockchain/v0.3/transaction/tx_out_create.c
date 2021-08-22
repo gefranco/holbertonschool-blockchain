6e1544019064c52ceb9954c92b49d9def0dea444
gefranco <933@holbertonschool.com>
2021-08-22 22:37:10 +0000

    [fix]betty

--- blockchain/v0.3/transaction/tx_out_create.c
+++ blockchain/v0.3/transaction/tx_out_create.c
@@ -1,14 +1,20 @@
 #include "transaction.h"
 #include <string.h>
+/**
+ * tx_out_create - allocates and initializes a transaction output structure
+ * @amount: is the amount of the transaction
+ * @pub: is the public key of the transaction receiver
+ * Return: a pointer to the created transaction output, or NULL upon failure
+ */
 tx_out_t *tx_out_create(uint32_t amount, uint8_t const pub[EC_PUB_LEN])
 {
 	tx_out_t *tx_out;
-	
+
 	if (!pub)
 		return (NULL);
 	tx_out = calloc(1, sizeof(tx_out_t));
 	if (!tx_out)
-		return(NULL);
+		return (NULL);
 
 	tx_out->amount = amount;
 	memcpy(tx_out->pub, pub, EC_PUB_LEN);
