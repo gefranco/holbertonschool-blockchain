6cd2394feaeab660ccc2296007d78d6109d9604e
gefranco <933@holbertonschool.com>
2021-08-23 23:34:35 +0000

    [update] blockchain_create & blockchaindestroy

---
+++ blockchain/v0.3/blockchain_create.c
@@ -0,0 +1,51 @@
+#include "blockchain.h"
+#include <stdlib.h>
+#include <string.h>
+
+#define TIMESTAMP 1537578000
+/**
+ * blockchain_create - creates a Blockchain structure, and initializes it
+ *
+ * Return: a new Blockchain or NULL
+ */
+blockchain_t *blockchain_create(void)
+{
+
+
+
+
+	blockchain_t *blockchain = NULL;
+	block_t *block = NULL;
+	llist_t *list = NULL;
+	llist_t *unspent = llist_create(MT_SUPPORT_TRUE);	
+
+	blockchain = calloc(1, sizeof(blockchain_t));
+	block = calloc(1, sizeof(block_t));
+	list = llist_create(MT_SUPPORT_TRUE);
+
+	if (!blockchain || !block || !list)
+	{
+		free(blockchain);
+		free(block);
+		llist_destroy(list, 1, NULL);
+		return (NULL);
+	}
+	block->info.timestamp = TIMESTAMP;
+	memcpy(block->hash, GENESIS_HASH, SHA256_DIGEST_LENGTH);
+
+	memcpy(block->data.buffer, "Holberton School", 16);
+	block->data.len = 16;
+
+	if (llist_add_node(list, block, ADD_NODE_FRONT) == -1)
+	{
+		free(blockchain);
+		free(block);
+		llist_destroy(list, 1, NULL);
+		return (NULL);
+	}
+
+	blockchain->chain = list;
+	blockchain->unspent = unspent;
+	return (blockchain);
+
+}
---
+++ blockchain/v0.3/blockchain_destroy.c
@@ -0,0 +1,13 @@
+#include "blockchain.h"
+#include <stdlib.h>
+/**
+ * blockchain_destroy - deletes the blockchain
+ * @blockchain: blockchain to delete
+ */
+void blockchain_destroy(blockchain_t *blockchain)
+{
+	if (!blockchain)
+		return;
+	llist_destroy(blockchain->chain, 1, NULL);
+	free(blockchain);
+}
