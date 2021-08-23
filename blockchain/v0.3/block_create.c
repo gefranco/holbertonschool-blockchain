0d3cad9b934f596893fd42268c0868ae3d146519
gefranco <933@holbertonschool.com>
2021-08-23 23:01:45 +0000

    [update]block_create and block_destroy

---
+++ blockchain/v0.3/block_create.c
@@ -0,0 +1,36 @@
+#include "blockchain.h"
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+block_t *block_create(block_t const *prev,
+			int8_t const *data, uint32_t data_len)
+{
+	block_t *block;
+	uint32_t len;
+
+	block = calloc(1, sizeof(block_t));
+
+	if (!block || !prev || !data)
+	{
+		return (NULL);
+	}
+
+	len = data_len > BLOCKCHAIN_DATA_MAX ?
+		BLOCKCHAIN_DATA_MAX : data_len;
+
+	memcpy(block->data.buffer, data, len);
+	block->data.len = len;
+
+	memcpy(block->info.prev_hash, prev->hash, SHA256_DIGEST_LENGTH);
+	block->info.index = prev->info.index + 1;
+	block->info.timestamp = (uint64_t)time(NULL);
+	
+	block->transactions = llist_create(MT_SUPPORT_FALSE);
+	if (!block->transactions)
+	{
+		free(block);
+		return (NULL);
+	}	
+	
+	return (block);
+}
---
+++ blockchain/v0.3/block_destroy.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include "blockchain.h"
+/**
+ * block_destroy - free the block
+ * @block: block to free
+ */
+void block_destroy(block_t *block)
+{
+	llist_destroy(block->transactions, 1, (node_dtor_t)transaction_destroy);
+	free(block);
+}
