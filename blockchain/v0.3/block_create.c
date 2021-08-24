6d6eccd66614f83e0a674eb62930dd30b6b1df13
gefranco <933@holbertonschool.com>
2021-08-24 00:59:56 +0000

    [testing]

--- blockchain/v0.3/block_create.c
+++ blockchain/v0.3/block_create.c
@@ -5,32 +5,25 @@
 block_t *block_create(block_t const *prev,
 			int8_t const *data, uint32_t data_len)
 {
+
 	block_t *block;
-	uint32_t len;
+	uint32_t max_len = data_len > BLOCKCHAIN_DATA_MAX ?
+		   BLOCKCHAIN_DATA_MAX : data_len;
 
-	block = calloc(1, sizeof(block_t));
-
-	if (!block || !prev || !data)
-	{
+	if (!prev || !data)
 		return (NULL);
-	}
-
+	block = calloc(1, sizeof(*block));
+	if (!block)
+		return (NULL);
 	block->transactions = llist_create(MT_SUPPORT_FALSE);
-        if (!block->transactions)
-        {
-                free(block);
-                return (NULL);
-        }
-
-	len = data_len > BLOCKCHAIN_DATA_MAX ?
-		BLOCKCHAIN_DATA_MAX : data_len;
-
-	memcpy(block->data.buffer, data, len);
-	block->data.len = len;
+	if (!block->transactions)
+		return (free(block), NULL);
+	memcpy(block->data.buffer, data, max_len);
+	block->data.len = max_len;
 
 	memcpy(block->info.prev_hash, prev->hash, SHA256_DIGEST_LENGTH);
 	block->info.index = prev->info.index + 1;
 	block->info.timestamp = (uint64_t)time(NULL);
-	
+
 	return (block);
 }
