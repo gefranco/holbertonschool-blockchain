4f6b88a24bfbb85fbdab91b2bc39ea73b63c1229
gefranco <933@holbertonschool.com>
2021-08-24 17:04:32 +0000

    [testing]

--- blockchain/v0.3/transaction/transaction_is_valid.c
+++ blockchain/v0.3/transaction/transaction_is_valid.c
@@ -1,98 +1,50 @@
 #include "transaction.h"
 #include <string.h>
-int match_unspent(llist_node_t unspent, void *arg)
-{
-	unspent_tx_out_t *unspent_out = unspent;
-	tx_in_t *tx_in = arg;
 
-	if (memcmp(tx_in->tx_out_hash,
-			unspent_out->out.hash,
-			SHA256_DIGEST_LENGTH) == 0
-	&& memcmp(tx_in->block_hash,
-			unspent_out->block_hash,
-			SHA256_DIGEST_LENGTH) == 0
-	&& memcmp(tx_in->tx_id,
-			unspent_out->tx_id,
-			SHA256_DIGEST_LENGTH) == 0	
+unspent_tx_out_t *find_unspent_output(llist_t *all_unspent, tx_in_t *in)
+{
+	int i, size;
+	unspent_tx_out_t *tmp_unspent;
+
+	for (i = 0, size = llist_size(all_unspent); i < size; i++)
+	{
+		tmp_unspent = llist_get_node_at(all_unspent, i);
+		if (
+			!memcmp(tmp_unspent->out.hash, in->tx_out_hash, sizeof(tmp_unspent->out.hash)) &&
+			!memcmp(tmp_unspent->block_hash, in->block_hash, sizeof(tmp_unspent->block_hash))
+			)
+			return (tmp_unspent);
+	}
+
+	return (NULL);
+}
+
+int transaction_is_valid(transaction_t const *transaction, llist_t *all_unspent)
+{
+	uint8_t hash_buf[SHA256_DIGEST_LENGTH];
+	int i, size;
+	uint32_t unspent_total = 0, total = 0;
+	unspent_tx_out_t *tmp_unspent;
+	tx_in_t *tmp_input;
+
+	if (!transaction ||
+		!transaction_hash(transaction, hash_buf) ||
+		memcmp(hash_buf, transaction->id, sizeof(hash_buf))
 	)
-		return (1);
-	return (0);
-}
-
-int exist_unspent_output(llist_node_t tx_input, unsigned int idx, void *arg)
-{
-	tx_in_t *tx_in = tx_input;
-	valid_transaction_t *valid_tx = arg;
-	unspent_tx_out_t *unspent_tx_out = llist_find_node(
-						valid_tx->all_unspent, match_unspent, tx_in);
-
-	EC_KEY *key;
-
-	(void)idx;
-	if (!unspent_tx_out)
-	{
-		valid_tx->is_valid = 0;
-		printf("no utxo");
-		return (1);
-	}
-	key = ec_from_pub(unspent_tx_out->out.pub);
-	if (!key || !ec_verify(key,
-				valid_tx->tx->id,
-				SHA256_DIGEST_LENGTH,
-				&tx_in->sig))
-	{
-		valid_tx->is_valid = 0;
-		EC_KEY_free(key);
-		printf("invalid sign");
-		return (1);
-	}
-
-	EC_KEY_free(key);
-	valid_tx->in_amount += unspent_tx_out->out.amount;
-	return (0);
-
-}
-
-int sum_output(llist_node_t tx_out, unsigned int idx, void *arg)
-{
-	tx_out_t *tx = tx_out;
-	valid_transaction_t *valid_tx = arg;
-
-	(void)idx;
-	valid_tx->out_amount += tx->amount;
-	return (0);
-}
-
-int transaction_is_valid(transaction_t const *transaction,
-		llist_t *all_unspent)
-{
-	uint8_t tx_hash[SHA256_DIGEST_LENGTH];
-
-	valid_transaction_t valid_transaction = {0};
-
-	valid_transaction.tx = transaction;
-	valid_transaction.all_unspent = all_unspent;
-	valid_transaction.is_valid = 1;
-	valid_transaction.in_amount = 0;
-	valid_transaction.out_amount = 0;
-
-	transaction_hash(transaction, tx_hash);
-
-	if (memcmp(transaction->id, tx_hash, SHA256_DIGEST_LENGTH) != 0)
-	{
-		printf("Invalid hash");
-		return (0);
-	}
-	if (llist_for_each(transaction->inputs,
-				exist_unspent_output,
-				&valid_transaction))
 		return (0);
 
-	llist_for_each(transaction->outputs, sum_output, &valid_transaction);
-
-	if (valid_transaction.in_amount != valid_transaction.out_amount)
+	for (i = 0, size = llist_size(transaction->inputs); i < size; i++)
 	{
-		return (0);
+		tmp_input = llist_get_node_at(transaction->inputs, i);
+		if (!(tmp_unspent = find_unspent_output(all_unspent, tmp_input)) ||
+			!ec_verify(ec_from_pub(tmp_unspent->out.pub), transaction->id, SHA256_DIGEST_LENGTH, &tmp_input->sig)
+		)
+			return (0);
+		unspent_total += tmp_unspent->out.amount;
 	}
-	return (1);
+
+	for (i = 0, size = llist_size(transaction->outputs); i < size; i++)
+		total += ((tx_out_t *)(llist_get_node_at(transaction->outputs, i)))->amount;
+
+	return (total == unspent_total);
 }
