63598b7f63b22eb1902548c1fe0824ef8cffa282
gefranco <933@holbertonschool.com>
2021-08-24 17:38:07 +0000

    [testing]

--- blockchain/v0.3/transaction/transaction_destroy.c
+++ blockchain/v0.3/transaction/transaction_destroy.c
@@ -10,7 +10,8 @@ void transaction_destroy(transaction_t *transaction)
 	if (!transaction)
 		return;
 
-	if (transaction->inputs) llist_destroy(transaction->inputs, 1, free);
-	if (transaction->outputs) llist_destroy(transaction->outputs, 1, free);
+	llist_destroy(transaction->inputs, 1, free);
+
+	llist_destroy(transaction->outputs, 1, free);
 	free(transaction);
 }
