4042f507974852c5238cea32eeb3e238103d1765
gefranco <933@holbertonschool.com>
2021-08-26 23:06:26 +0000

    [add] deserialize transactions

--- blockchain/v0.3/blockchain_deserialize.c
+++ blockchain/v0.3/blockchain_deserialize.c
@@ -2,8 +2,103 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
 void _swap_endian(void *p, size_t size);
 int8_t	_get_endianness(void);
+
+
+llist_t *read_outputs(FILE *file, uint8_t swap_endian, int num_outputs)
+{
+	llist_t *outputs;
+	tx_out_t *out;
+	
+	if (num_outputs <= 0)
+		return NULL;
+	outputs = llist_create(MT_SUPPORT_FALSE);
+	
+	if (!outputs)
+		return (NULL);
+
+	while ((int) num_outputs-- > 0)
+	{
+		out = calloc(1, sizeof(tx_out_t));
+		llist_add_node(outputs, out, ADD_NODE_REAR);
+		fread(&out->amount, sizeof(out->amount), 1, file);
+		fread(&out->pub, sizeof(out->pub), 1, file);
+		fread(&out->hash, sizeof(out->hash), 1, file);
+		if (swap_endian)
+			_swap_endian(&out->amount, sizeof(out->amount));
+	}
+
+	return (outputs);
+}
+
+llist_t *read_inputs(FILE *file, uint8_t swap_endian, int num_inputs)
+{
+	llist_t *inputs;
+	tx_in_t *in;
+	if(num_inputs <= 0)
+		return NULL;
+	
+	inputs = llist_create(MT_SUPPORT_FALSE);
+	if (!inputs)
+		return NULL;
+	
+	while ((int)num_inputs-- > 0)
+	{
+		in = calloc(1, sizeof(tx_in_t));
+		llist_add_node(inputs, in, ADD_NODE_REAR);
+		
+		fread(&in->block_hash, sizeof(in->block_hash), 1, file);
+		fread(&in->tx_id, sizeof(in->tx_id), 1, file);
+		fread(&in->tx_out_hash, sizeof(in->tx_out_hash), 1, file);
+		fread(&in->sig.sig, sizeof(in->sig.sig), 1, file);
+		fread(&in->sig.len, sizeof(in->sig.len), 1, file);
+		if (swap_endian)
+                        _swap_endian(&in->sig.len, sizeof(in->sig.len));
+	}
+	return inputs;
+}
+
+llist_t *read_transactions(FILE *file, uint8_t swap_endian)
+{
+	uint32_t num_txs, num_inputs, num_outputs;
+	transaction_t *tx;
+	llist_t *list;
+	
+	fread(&num_txs, sizeof(num_txs), 1, file);
+
+	list = llist_create(MT_SUPPORT_FALSE);
+	
+	if(!list)
+		return (NULL);
+	if (swap_endian)
+                        _swap_endian(&num_txs, sizeof(num_txs));
+	if((int)num_txs <= 0)
+	{
+		llist_destroy(list, 0, NULL);
+		return (NULL);
+	}
+	
+	while (num_txs-- > 0)
+	{
+		tx = calloc(1, sizeof(transaction_t));
+		llist_add_node(list, tx, ADD_NODE_REAR);
+		fread(&tx->id, sizeof(tx->id), 1, file);
+		fread(&num_inputs, sizeof(num_inputs),1, file);
+		if (swap_endian)
+			_swap_endian(&num_inputs, sizeof(num_inputs));
+		fread(&num_outputs, sizeof(num_outputs), 1, file);
+		if (swap_endian)
+			_swap_endian(&num_outputs, sizeof(num_outputs));
+		
+		
+		tx->inputs = read_inputs(file, swap_endian, num_inputs);
+		tx->outputs = read_outputs(file, swap_endian, num_outputs);
+	}	
+	return (NULL);
+}
+
 
 /**
  * read_blocks - Entry point
@@ -41,6 +136,8 @@ int read_blocks(FILE *file, uint8_t swap_endian,
 			_swap_endian(&block->info.nonce, sizeof(block->info.nonce));
 		}
 		block->data.len = data_len;
+		block->transactions = read_transactions(file, swap_endian);
+	
 		llist_add_node(list, block, ADD_NODE_REAR);
 
 	}
