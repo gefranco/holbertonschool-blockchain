c4496bf411822c42d1b57006dc89263aa469b357
gefranco <933@holbertonschool.com>
2021-08-28 16:08:25 +0000

    [fix]rfctring

--- blockchain/v0.3/transaction/transaction_is_valid.c
+++ blockchain/v0.3/transaction/transaction_is_valid.c
@@ -1,50 +1,74 @@
 #include "transaction.h"
 #include <string.h>
 
-unspent_tx_out_t *find_unspent_output(llist_t *all_unspent, tx_in_t *in)
+/**
+ * find_unspent_output - finds unspent output
+ * @all_unspent: unspent tx ouput list
+ * @tx_in: transaction input
+ * Return: EXIT_SUCCESS or EXIT_FAILURE
+ */
+unspent_tx_out_t *find_unspent_output(llist_t *all_unspent, tx_in_t *tx_in)
 {
 	int i, size;
-	unspent_tx_out_t *tmp_unspent;
+	unspent_tx_out_t *utx_out;
 
-	for (i = 0, size = llist_size(all_unspent); i < size; i++)
+	size = llist_size(all_unspent);
+	for (i = 0; i < size; i++)
 	{
-		tmp_unspent = llist_get_node_at(all_unspent, i);
-		if (
-			!memcmp(tmp_unspent->out.hash, in->tx_out_hash, sizeof(tmp_unspent->out.hash)) &&
-			!memcmp(tmp_unspent->block_hash, in->block_hash, sizeof(tmp_unspent->block_hash))
-			)
-			return (tmp_unspent);
+		utx_out = llist_get_node_at(all_unspent, i);
+		if (!memcmp(utx_out->out.hash,
+			tx_in->tx_out_hash,
+			sizeof(utx_out->out.hash) == 0) &&
+			!memcmp(utx_out->block_hash,
+				tx_in->block_hash,
+				sizeof(utx_out->block_hash) == 0)
+		)
+			return (utx_out);
 	}
-
 	return (NULL);
 }
 
-int transaction_is_valid(transaction_t const *transaction, llist_t *all_unspent)
+int transaction_is_valid(transaction_t const *transaction,
+		llist_t *all_unspent)
 {
-	uint8_t hash_buf[SHA256_DIGEST_LENGTH];
+	uint8_t hash[SHA256_DIGEST_LENGTH];
 	int i, size;
-	uint32_t unspent_total = 0, total = 0;
-	unspent_tx_out_t *tmp_unspent;
-	tx_in_t *tmp_input;
+	uint32_t total_unspent, total;
+	unspent_tx_out_t *utx_out;
+	tx_in_t *tx_in;
+	tx_out_t *tx_out;
 
+	total = 0;
+	total_unspent = 0;
 	if (!transaction ||
-		!transaction_hash(transaction, hash_buf) ||
-		memcmp(hash_buf, transaction->id, sizeof(hash_buf))
+		!transaction_hash(transaction, hash) ||
+		memcmp(hash, transaction->id, sizeof(hash))
 	)
 		return (0);
 
-	for (i = 0, size = llist_size(transaction->inputs); i < size; i++)
+	size = llist_size(transaction->inputs);
+	for (i = 0; i < size; i++)
 	{
-		tmp_input = llist_get_node_at(transaction->inputs, i);
-		if (!(tmp_unspent = find_unspent_output(all_unspent, tmp_input)) ||
-			!ec_verify(ec_from_pub(tmp_unspent->out.pub), transaction->id, SHA256_DIGEST_LENGTH, &tmp_input->sig)
-		)
+		tx_in = llist_get_node_at(transaction->inputs, i);
+		utx_out = find_unspent_output(all_unspent, tx_in);
+
+		if (!utx_out ||
+			!ec_verify(ec_from_pub(utx_out->out.pub),
+				transaction->id, SHA256_DIGEST_LENGTH, &tx_in->sig))
+		{
 			return (0);
-		unspent_total += tmp_unspent->out.amount;
+
+		}
+		total_unspent += utx_out->out.amount;
+
 	}
 
-	for (i = 0, size = llist_size(transaction->outputs); i < size; i++)
-		total += ((tx_out_t *)(llist_get_node_at(transaction->outputs, i)))->amount;
+	size = llist_size(transaction->outputs);
 
-	return (total == unspent_total);
+	for (i = 0; i < size; i++)
+	{
+		tx_out = llist_get_node_at(transaction->outputs, i);
+		total += tx_out->amount;
+	}
+	return (total_unspent == total);
 }
